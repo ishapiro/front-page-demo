719c2ebe2b324fec67375d9ca1750ab40c1490ca
murtaza.husain <murtaza.husain@golpik.com>
2022-11-10 21:54:28 +0500

    amplify variables

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,27 +1,239 @@
 <template>
   <div class="profile-page pb-16">
-    <v-container class="">
+    <v-container>
       <v-row>
         <v-col>
-          <div
-            class="text-h3 primary--text font-weight-bold text-center text-sm-left pb-8"
+          <v-card class="px-16 mx-auto mt-16 pb-16 pt-10"
+            max-width="1370"
           >
-            Premium Plans
-            <div class="secondary--text font-weight-bold text-h4 mt-3">Coming Soon</div>
-          </div>
-          <div class="text-h6">
-            Your basic membership is free forever. As a premium member you gain access to verified model templates, downloads of Model Templates (OpenSCAD scripts), the ability to save template settings, private model storage, branded Creator Pages and access to advanced Creator tools.
-            
-            <div class="my-8">Premium features are free for everyone while the application is in beta.</div>
-            
-            <span class="font-italic">THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</span>
-          </div>
+            <v-row class="">
+              <v-col
+                cols="12"
+                md="7"
+                class="d-flex align-center"
+                >
+                  <div
+                    class="text-h3 primary--text font-weight-bold text-center text-sm-left"
+                    elevation
+                  >
+                    Account Management
+                  </div>
+                </v-col>
+                <v-col
+                cols="12"
+                md="5"
+                class=""
+                >
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    <v-icon class="primary--text pe-5" v-bind="attrs" v-on="on">
+                      mdi-account
+                    </v-icon>
+                    info@makewithtech.com
+                  </div>
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    <v-icon class="primary--text pe-5" v-bind="attrs" v-on="on">
+                      mdi-card-account-details
+                    </v-icon>
+                    ee58fe86-a78c-49b6-828c-902d193774eb
+                  </div>
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    <v-icon class="primary--text pe-5" v-bind="attrs" v-on="on">
+                      mdi-email
+                    </v-icon>
+                    info@makewithtech.com
+                  </div>
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    <v-icon class="primary--text pe-5" v-bind="attrs" v-on="on">
+                      mdi-bell
+                    </v-icon>
+                    No subscription found
+                  </div>
+                </v-col>
+            </v-row>
+            <v-form v-model="valid">
+              <v-row class=" mt-16">
+                <v-col 
+                cols="12"
+                md="6"
+                class=""
+                >
+                  <div
+                    class="text-h6 font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    Nickname 
+                  </div>
+                  <v-text-field
+                    v-model="firstname"
+                    :rules="nameRules"
+                    label="Xyz"
+                    outlined
+                  ></v-text-field>
+                </v-col>
+                <v-col 
+                cols="12"
+                md="6"
+                class=""
+                >
+                  <div
+                    class="text-h6 font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    Website URL
+                  </div>
+                  <v-text-field
+                    v-model="firstname"
+                    :rules="webRules"
+                    label="www.makewithtech.com"
+                    outlined
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row class="">
+                <v-col 
+                cols="12"
+                class=""
+                >
+                  <div
+                    class="text-h6 font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    About You 
+                  </div>
+                  <v-textarea
+                    label="Enter Your Information"
+                    auto-grow
+                    outlined
+                    rows="10"
+                    row-height="15"
+                  ></v-textarea>
+                </v-col>
+                <v-col
+                cols="12" 
+                class=""
+                >
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left"
+                  >
+                    Sign up for Buy Me a Coffee and enter your ID to receive tips when your models are downloaded from the results list. Your ID is the string after the Buy Me a Coffee widget URL. For example in the following case: 
+                  </div>
+                  <div
+                    class="font-weight-bold text-center text-sm-left"
+                  >
+                    <v-btn 
+                      href="https://www.google.com/"
+                      plain
+                      class="black--text"
+                    >
+                      www.buymeacoffee.com/makewithtech
+                    </v-btn>
+                  </div>
+                  <div>
+                    "<span class="font-weight-bold secondary--text">makewithtech</span>" is your ID.
+                  </div>
+                </v-col>
+                <v-col 
+                cols="12"
+                md="6"
+                class=""
+                >
+                  <div
+                    class="text-h6 font-weight-bold text-center text-sm-left pb-2"
+                  >
+                    Buy me a coffee 
+                  </div>
+                  <v-text-field
+                    v-model="id"
+                    :rules="idrules"
+                    label="ID"
+                    outlined
+                  ></v-text-field>
+                </v-col>
+                <v-col
+                cols="12" 
+                class=""
+                >
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left"
+                  >
+                    Sign up for Buy Me a Coffee and enter your ID to receive tips when your models are downloaded from the results list. Your ID is the string after the Buy Me a Coffee widget URL. For example in the following case: 
+                  </div>
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-checkbox
+                    v-model="checkbox"
+                    label="Try experimental features"
+                  ></v-checkbox>
+                  <v-btn 
+                    x-large
+                    class="grey darken-3 white--text"
+                  >
+                    Save/Update Account
+                  </v-btn>
+                  <div
+                    class="primary--text font-weight-bold text-center text-sm-left py-5"
+                  >
+                  Changing the layout will redirect to the home page 
+                  </div>
+                  <v-select
+                    :items="itemsselect"
+                    label="Auto"
+                    outlined
+                  ></v-select>
+                </v-col>
+                  <v-col
+                  cols="12"
+                  md="6"
+                >
+                  <v-img
+                    max-width="490"
+                    src="@/assets/profile-down.png"
+                    class="ms-auto"
+                  ></v-img>
+                </v-col>
+                  <v-btn 
+                    block
+                    x-large
+                    class="secondary white--text"
+                  >
+                    SIGN OUT
+                  </v-btn>
+              </v-row>
+            </v-form>
+          </v-card>
+        </v-col>
+        <v-col
+        cols="12"
+        class="d-flex"
+        >
+          <v-btn 
+            x-large
+            class="primary white--text mx-auto mt-16 mb-8"
+          >
+            Update to a Premium Subscription?
+          </v-btn>
         </v-col>
       </v-row>
+      
     </v-container>
   </div>
 </template>
 <style>
+:root {
+  --amplify-primary-color: #ff7500;
+  --amplify-primary-tint: #3f6060;
+  /* --amplify-primary-shade: #2dba89; */
+  /* --amplify-background-color: #e0faf4; */
+}
+
 .profile-page{
   background: url('@/assets/bg-profile.png') no-repeat center center / cover;
   background-size: cover;
@@ -31,19 +243,29 @@
 import { onAuthUIStateChange } from "@aws-amplify/ui-components";
 
 export default {
+  data: () => ({
+      valid: false,
+      firstname: '',
+      lastname: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+      ],
+      webRules: [
+        v => !!v || 'Website is required',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      itemsselect: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+    }),
   name: "AuthStateApp",
   created() {
     this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
       this.authState = authState;
       this.user = authData;
     });
-  },
-  data() {
-    return {
-      user: undefined,
-      authState: undefined,
-      unsubscribeAuth: undefined,
-    };
   },
   beforeDestroy() {
     this.unsubscribeAuth();
